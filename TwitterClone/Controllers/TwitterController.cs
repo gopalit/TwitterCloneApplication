9eb2f9d1c7dd2250fd642be53e29241ea37eecb1
Gopal <gopalit@yahoo.com>
2019-01-02 22:59:34 -0700

    Add project files.

---
+++ TwitterClone/Controllers/TwitterController.cs
@@ -0,0 +1,133 @@
+﻿using BusinessLayer;
+using DataAccessLayer;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TwitterClone.Models;
+
+namespace TwitterClone.Controllers
+{
+    public class TwitterController : Controller
+    {
+        private PersonManager personManager = new PersonManager();
+
+        // GET: Twitter
+        public ActionResult Tweet()
+        {
+            if (Session["UserName"] != null)
+            {
+                string userId = Session["UserName"].ToString();
+                TweetViewModel tweetViewModel = new TweetViewModel();
+                tweetViewModel.Tweets = new Collection<TweetModel>();
+                Collection<Tweet> tweets = personManager.GetFollowingTweets(userId);
+                tweetViewModel.FollowersCount = personManager.GetFollowers(userId).Count;
+                tweetViewModel.FollowingCount = personManager.GetFollowings(userId).Count;
+                tweets.ToList().ForEach(x =>
+                tweetViewModel.Tweets.Add(new TweetModel()
+                {
+                    TweetId = x.Tweet_Id,
+                    UserId = x.User_Id,
+                    Message = x.Message,
+                    CreatedDate = x.Created
+                }));
+                tweetViewModel.IsTweetPage = true;
+                return View(tweetViewModel);
+            }
+            else
+            {
+                return RedirectToAction("Login", "Account");
+            }
+        }
+
+        // Commments
+        // POST: /Account/Register
+        [HttpPost]
+        [AllowAnonymous]
+        public ActionResult AddTweet(string message)
+        {
+            try
+            {
+                if (Session["UserName"] != null)
+                {
+                    Tweet tweet = new Tweet
+                    {
+                        User_Id = Session["UserName"].ToString(),
+                        Message = message,
+                        Created = DateTime.Now
+                    };
+
+                    personManager.SaveTweet(tweet);
+
+                }
+                else
+                {
+                    Session["LoginModel"] = null;
+                    Session["UserName"] = null;
+                    return RedirectToAction("Login", "Account");
+                }
+
+            }
+            catch (Exception)
+            {
+                AddErrors("Problem with our end. Please try again later..");
+            }
+            return RedirectToAction("Tweet", "Twitter");
+        }
+
+        public ActionResult GetUsers(string type)
+        {
+            List<FollowingModel> lst = new List<FollowingModel>();
+            try
+            {
+                if (Session["UserName"] != null)
+                {
+                    string userId = Session["UserName"].ToString();
+
+
+
+                    if (type == "follow")
+                    {
+                        var userList = personManager.GetFollowers(userId);
+                        userList.ToList().ForEach(x =>
+                lst.Add(new FollowingModel()
+                {
+                    UserId = x.User_Id,
+                    FollowingId = x.Following_Id
+
+                }));
+                    }
+                    else
+                    {
+
+                        var userList = personManager.GetFollowings(userId);
+
+                        userList.ToList().ForEach(x =>
+                lst.Add(new FollowingModel()
+                {
+                    UserId = x.User_Id,
+                    FollowingId = x.Following_Id
+
+                }));
+                    }
+
+
+
+                }
+
+            }
+            catch (Exception)
+            {
+                AddErrors("Failed to fetch Followers. Please try again later..");
+            }
+            return Json(lst, JsonRequestBehavior.AllowGet);
+        }
+
+        private void AddErrors(string error)
+        {
+            ModelState.AddModelError("", error);
+        }
+    }
+}
---
+++ TwitterClone/Models/TweetViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Web;
+
+namespace TwitterClone.Models
+{
+    public class TweetModel
+    {
+        public int TweetId { get; set; }
+        public string UserId { get; set; }
+        public string Message { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+
+    public class FollowingModel
+    {
+        public string UserId { get; set; }
+        public string FollowingId { get; set; }
+    }
+
+    public class TweetViewModel
+    {
+        public Collection<TweetModel> Tweets { get; set; }
+        public Collection<FollowingModel> Followings { get; set; }
+
+        public int FollowersCount { get; set; }
+
+        public int FollowingCount { get; set; }
+        public bool IsTweetPage { get; set; }
+    }
+
+}
